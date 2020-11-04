913b14109514345a20b52cec2b05b99317ef4059
Keith Miller <keithmiller@Keiths-iMac.local>
2020-11-04 16:40:36 -0500

    Added Private and public subnets and gitignore

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
-variable "cidr"{
-    default = "10.0.0.0/16"
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public-subnet" {
+  default = "10.0.0.0/24"
+}
+
+variable "private-subnet" {
+    default = "10.0.1.0/24"
 }
