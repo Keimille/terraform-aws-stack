478ee88b6bf9e5742f75c6ec72df61ef04f16e4d
Keith Miller <keithemmanuel@gmail.com>
2020-11-20 16:16:42 -0500

    Updated usage instructions to readme and added HTTPS to ingress rules

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "cidr" {
   description = "The CIDR of the VPC. This should be overwritten"
-  type = string 
-  default = "0.0.0.0/0"
+  type        = string
+  default     = "0.0.0.0/0"
 }
 
 variable "public-subnet" {
   description = "The CIDR of thr Public Subnet. This should be over written"
-  type = string
-  default = "0.0.0.0/0"
+  type        = string
+  default     = "0.0.0.0/0"
 }
 
 variable "private-subnet" {
   description = "The CIDR of the Public Subnet. This should be overwritten"
-  default = "0.0.0.0/0"
+  default     = "0.0.0.0/0"
 }
 
 variable "ami" {
@@ -23,14 +23,14 @@ variable "ami" {
 
 variable "instance-type" {
   description = "EC2 instance type. Can be overwritten but default is t2.micro"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "ec2-private-ip" {
   description = "Assign a private ip to ec2 instabceif desired. Must be over written and within a usable address range of private subnet"
-  type = string
-  default = "0.0.0.0"
+  type        = string
+  default     = "0.0.0.0"
 }
 
 variable "web-private-ip2" {
